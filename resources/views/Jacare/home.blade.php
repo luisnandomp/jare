da9b4bbac2e4763ddca076c0eb0818f5bfecf815
luisnandomp <luisnando.marques2107@gmail.com>
2023-03-17 20:36:41 -0300

    inicialização do vite

--- resources/views/Jacare/home.blade.php
+++ resources/views/Jacare/home.blade.php
@@ -4,11 +4,15 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
         <title>Pag inicial</title>
+        @vite(['resources/scss/app.scss'])
     </head>
     <body>
-        <h1>Cadastre seu Jaré conosco :D</h1>
+        <h1>Cadastre seu Jaré conosco :X</h1>
 
         <a href="{{route('jacare.create')}}">Cadastre aqui</a>
+
+        <a href="{{route('jacare.index')}}">Veja os jacarés já cadastrados</a>
     </body>
 </html>
